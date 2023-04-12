b6b66af7aea23b3374ab34ece48608c7ac002933
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-12 15:58:27 +0000

    Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,26 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render ({ :template => "game_templates/rules.html.erb"})
+  end
+
   def play_rock
     # redirect_to("https://www.google.com")
     render ({ :template => "game_templates/user_rock.html.erb"})
   end
 
   def play_paper
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    elsif @computer_move == "scissors"
+      @outcome = "lost"
+    end
+
     render ({ :template => "game_templates/user_paper.html.erb"})
   end
 
