f5a80e35d71ce72793d2f122a24937ccfe01d25c
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-12 15:43:11 +0000

    Dynamic Rock, Paper, Scissors Pages Created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
   def play_rock
-    redirect_to("https://www.google.com")
+    # redirect_to("https://www.google.com")
+    render ({ :template => "game_templates/user_rock.html.erb"})
+  end
+
+  def play_paper
+    render ({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    render ({ :template => "game_templates/user_scissors.html.erb"})
   end
 end
